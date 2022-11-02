d08597ae767714980e4d166b499c59c9545771af
antkowiak <5641713+antkowiak@users.noreply.github.com>
2022-11-02 07:46:38 -0500

    removed email

--- num2word.cpp
+++ num2word.cpp
@@ -3,13 +3,11 @@
  * num2word.cpp - Convert integer numbers (up to unsigned long long max)
  *                to english words.
  *
- * Created by and copyright: Ryan Antkowiak (antkowiak@gmail.com)
+ * Created by and copyright: Ryan Antkowiak
  *
  * Created on: 2016-06-22
  *
  * Free for educational and non-commercial use.
- *
- * For questions about a commercial license, contact antkowiak@gmail.com
  *
  * ********************************************************************** */
 
